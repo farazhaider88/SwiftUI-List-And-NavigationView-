899b7ffbfbdb8d6e4a37a56aa6f40ec94a5c1f83
FarazHaider <farazhaider88@yahoo.com>
2021-11-09 23:07:41 +0500

    Initial Commit

---
+++ ListAndNavigation/ListAndNavigation/ListAndNavigationApp.swift
@@ -0,0 +1,17 @@
+//
+//  ListAndNavigationApp.swift
+//  ListAndNavigation
+//
+//  Created by Faraz Haider on 09/11/2021.
+//
+
+import SwiftUI
+
+@main
+struct ListAndNavigationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
